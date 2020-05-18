66f78b6fc838c3e2adfe4956059217d3c708802f
Xavier Davis <xyd93@outlook.com>
2020-05-18 16:02:05 -0400

    Finish HTML for all elements except recipe item

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe("Pizza", "Tasty pizza", "https://media.gettyimages.com/photos/cheesy-pepperoni-pizza-picture-id938742222?s=612x612"),
+    new Recipe("Pizza", "Tasty pizza", "https://media.gettyimages.com/photos/cheesy-pepperoni-pizza-picture-id938742222?s=612x612")
+  ];
 
   constructor() { }
 
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
+
+  items: Ingredient[] = [
+    new Ingredient("Apples", 3),
+    new Ingredient("Onion", 6),
+    new Ingredient("Bread", 1)
+  ];
 
   constructor() { }
 
